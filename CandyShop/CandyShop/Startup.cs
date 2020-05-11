f05524e4da4f05d9bad45af2186a3c8f56a005ef
Michał Warchałowski <michalwarchalowski@gmail.com>
2020-05-11 20:13:29 +0200

    Styling List View

--- CandyShop/CandyShop/Startup.cs
+++ CandyShop/CandyShop/Startup.cs
@@ -18,7 +18,7 @@ namespace CandyShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation(); ;
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<ICandyRepository,CandyRepository> ();
         }
