2e4a099763691e974b34a190106682d581e75a12
Michał Warchałowski <michalwarchalowski@gmail.com>
2020-05-11 17:48:13 +0200

    Candy Controller

---
+++ CandyShop/CandyShop/Controllers/CandyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CandyShop.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CandyShop.Controllers
+{
+    public class CandyController : Controller
+    {
+        private readonly ICandyRepository _candyRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        public CandyController(ICandyRepository candyRepository, ICategoryRepository categoryRepository)
+        {
+            _candyRepository = candyRepository;
+            _categoryRepository = categoryRepository;
+        }
+    }
+}
