ca23b1417d3e030564b779d2b9be252d7a643f7d
Michał Warchałowski <michalwarchalowski@gmail.com>
2020-05-11 16:56:11 +0200

    Candy Model

---
+++ CandyShop/CandyShop/Models/Candy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShop.Models
+{
+    public class Candy
+    {
+        public int CandyId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public int ImageThumnailUrl { get; set; }
+        public bool IsOnSale { get; set; }
+        public bool IsOnStock { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
