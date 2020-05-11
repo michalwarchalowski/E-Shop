01bea4fcf9536b547df1100300ff0c6261f83015
Michał Warchałowski <michalwarchalowski@gmail.com>
2020-05-11 17:04:50 +0200

    Adding Interfaces

---
+++ CandyShop/CandyShop/Repository/IRepository/ICandyRepository.cs
@@ -0,0 +1,15 @@
+﻿using CandyShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShop.Repository.IRepository
+{
+    public interface ICandyRepository
+    {
+        IEnumerable<Candy> GetAllCandy { get; }
+        IEnumerable<Candy> GetCandyOnSale { get; }
+        Candy GetCandyById(int CandyId);
+    }
+}
---
+++ CandyShop/CandyShop/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using CandyShop.Models;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShop.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> GetAllCategories { get; }
+    }
+}
