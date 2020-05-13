c5cdc22134d55f304219c62d460d28efdfcc35df
Michał Warchałowski <michalwarchalowski@gmail.com>
2020-05-13 21:10:14 +0200

    CandyRepository AppDbCOntext

--- CandyShop/CandyShop/Repository/CandyRepository.cs
+++ CandyShop/CandyShop/Repository/CandyRepository.cs
@@ -1,8 +1,10 @@
 ﻿using CandyShop.Models;
 using CandyShop.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace CandyShop.Repository
@@ -10,31 +12,25 @@ namespace CandyShop.Repository
     public class CandyRepository : ICandyRepository
     {
         private readonly ICategoryRepository _categoryRepository = new CategoryRepository();
-
-
-        public IEnumerable<Candy> GetAllCandy => new List<Candy>
+        private readonly AppDbContext _appDbContext;
+        public CandyRepository(AppDbContext appDbContext)
         {
-            new Candy{CandyId=1,Name="Assorted hard Candy",Price=4.95M,Description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
-            "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make " +
-            "a type specimen book. It has survived not only five centuries, but also the leap into electronic",Category=_categoryRepository.GetAllCategories.ToList()[0],ImageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/HardCandy.jpg/220px-HardCandy.jpg",
-        IsOnStock=true,IsOnSale=false,ImageThumnailUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/HardCandy.jpg/220px-HardCandy.jpg"},
+            _appDbContext = appDbContext;
+        }
+        
+        public IEnumerable<Candy> GetAllCandy()
+        {
+            return _appDbContext.Candies.Include(c => c.Category);
+        }
 
-            new Candy{CandyId=2,Name="Assorted Chocolate Candy",Price=4.95M,Description="setting, remaining essentially unchanged. It was popularised in the 1960s with the release" +
-        " of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-        Category=_categoryRepository.GetAllCategories.ToList()[1],ImageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Chocolate_%28blue_background%29.jpg/200px-Chocolate_%28blue_background%29.jpg",
-        IsOnStock=true,IsOnSale=false,ImageThumnailUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Chocolate_%28blue_background%29.jpg/200px-Chocolate_%28blue_background%29.jpg"},
-
-               new Candy{CandyId=3,Name="Assorted Fruit Candy",Price=4.95M,Description="setting, remaining essentially unchanged. It was popularised in the 1960s with the release" +
-        " of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-        Category=_categoryRepository.GetAllCategories.ToList()[2],ImageUrl="https://upload.wikimedia.org/wikipedia/commons/1/10/Welch%27s_Fruit_Snacks_%284239096810%29.jpg",
-        IsOnStock=true,IsOnSale=false,ImageThumnailUrl="https://upload.wikimedia.org/wikipedia/commons/1/10/Welch%27s_Fruit_Snacks_%284239096810%29.jpg"}
-        };
-
-        public IEnumerable<Candy> GetCandyOnSale => throw new NotImplementedException();
+        public IEnumerable<Candy> GetCandyOnSale()
+        {
+            return _appDbContext.Candies.Include(c => c.Category).Where(x => x.IsOnSale);
+        }
 
         public Candy GetCandyById(int CandyId)
         {
-            return GetAllCandy.FirstOrDefault(x => x.CandyId == CandyId);
+            return _appDbContext.Candies.FirstOrDefault(x => x.CandyId == CandyId);
         }
     }
 }
