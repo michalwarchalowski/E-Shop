129d2ea02a6f44958820d49ce27ff7c5def8d46f
Michał Warchałowski <michalwarchalowski@gmail.com>
2020-05-13 22:16:45 +0200

    Add data to Database Migration

--- CandyShop/CandyShop/Migrations/20200513201255_addDataToDT.Designer.cs
+++ CandyShop/CandyShop/Migrations/20200513201255_addDataToDT.Designer.cs
@@ -3,14 +3,16 @@ using CandyShop.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CandyShop.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200513201255_addDataToDT")]
+    partial class addDataToDT
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
