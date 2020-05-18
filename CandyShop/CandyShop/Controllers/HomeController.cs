d0d315ee12cf8081abf9f31af56d88dfd3d0b186
Michał Warchałowski <michalwarchalowski@gmail.com>
2020-05-18 21:56:42 +0200

    HomeController

---
+++ CandyShop/CandyShop/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CandyShop.Repository.IRepository;
+using CandyShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CandyShop.Controllers
+{
+    public class HomeController : Controller
+
+        
+   
+    {
+        private readonly ICandyRepository _candyRepository;
+
+        public HomeController(ICandyRepository candyRepository)
+        {
+            _candyRepository = candyRepository;
+        }
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel();
+            homeViewModel.CandyOnSale = _candyRepository.GetCandyOnSale();
+         
+            return View(homeViewModel);
+        }
+    }
+}
