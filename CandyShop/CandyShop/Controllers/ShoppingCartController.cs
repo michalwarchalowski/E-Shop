6b5e53bb51bc199e11c977dc43bd385756d8c1dc
Michał Warchałowski <michalwarchalowski@gmail.com>
2020-05-27 23:09:56 +0200

    Shopping Cart Controller

---
+++ CandyShop/CandyShop/Controllers/ShoppingCartController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CandyShop.Models;
+using CandyShop.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CandyShop.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly ICandyRepository _candyRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartController(ICandyRepository candyRepository, ShoppingCart shoppingCart)
+        {
+            _candyRepository = candyRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        public ViewResult Index()
+        {
+            _shoppingCart.ShoppingCartItems = _shoppingCart.GetShoppingCartItems();
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(shoppingCartViewModel);
+        
+        }
+    }
+}
