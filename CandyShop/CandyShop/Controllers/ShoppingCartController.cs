7a05133e340ee3901f53bd9031de2a57f482ac5e
Michał Warchałowski <michalwarchalowski@gmail.com>
2020-05-27 23:19:34 +0200

    remove from shopping cart

--- CandyShop/CandyShop/Controllers/ShoppingCartController.cs
+++ CandyShop/CandyShop/Controllers/ShoppingCartController.cs
@@ -46,5 +46,19 @@ namespace CandyShop.Controllers
 
             return RedirectToAction("Index");
         }
+
+
+        public RedirectToActionResult RemoveFromShoppingCart(int candyId)
+        {
+
+            var selectedCandy = _candyRepository.GetAllCandy().FirstOrDefault(c => c.CandyId == candyId);
+
+            if (selectedCandy != null)
+            {
+                _shoppingCart.RemoveFromCart(selectedCandy);
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
