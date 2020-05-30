07544cf73b7b07459d596980cc412684bd79e7e0
Michał Warchałowski <michalwarchalowski@gmail.com>
2020-05-30 08:49:38 +0200

    Order Repository

---
+++ CandyShop/CandyShop/Repository/IRepository/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using CandyShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShop.Repository.IRepository
+{
+    public interface IOrderRepository
+    {
+        void CreateOrder(Order order);
+    }
+}
