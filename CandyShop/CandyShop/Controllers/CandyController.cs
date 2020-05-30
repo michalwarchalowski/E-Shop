f86b410ce2a3035ae8104f3e2f8cd628b4e3c92c
Michał Warchałowski <michalwarchalowski@gmail.com>
2020-05-30 08:00:30 +0200

    Display Candy for selected categroy

--- CandyShop/CandyShop/Controllers/CandyController.cs
+++ CandyShop/CandyShop/Controllers/CandyController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CandyShop.Models;
 using CandyShop.Repository.IRepository;
 using CandyShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
@@ -19,14 +20,36 @@ namespace CandyShop.Controllers
         }
 
 
-        public IActionResult List()
+        public IActionResult List(string category)
         {
             //ViewBag.CurrentCategory = "Bestsellers";
             //return View(_candyRepository.GetAllCandy);
-            var candyListViewModel = new CandyListViewModel();
-            candyListViewModel.Candies = _candyRepository.GetAllCandy();
-            candyListViewModel.CurrentCategory = "Bestsellers";
-            return View(candyListViewModel);
+            //  var candyListViewModel = new CandyListViewModel();
+            // candyListViewModel.Candies = _candyRepository.GetAllCandy();
+            // candyListViewModel.CurrentCategory = "Bestsellers";
+            // return View(candyListViewModel);
+
+            IEnumerable<Candy> candies;
+            string currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                candies = _candyRepository.GetAllCandy().OrderBy(c => c.CandyId);
+                currentCategory = "All Candy";
+            }
+            else
+            {
+                candies = _candyRepository.GetAllCandy().Where(c => c.Category.CategoryName == category);
+                currentCategory = _categoryRepository.GetAllCategories().FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new CandyListViewModel
+            {
+                Candies = candies,
+                CurrentCategory = currentCategory
+            });
+
+
         }
 
 
