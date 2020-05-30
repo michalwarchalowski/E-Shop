16de889b26eba0e211afacfcac9c097799ae32dc
Michał Warchałowski <michalwarchalowski@gmail.com>
2020-05-30 13:18:30 +0200

    CheckOut form and view

---
+++ CandyShop/CandyShop/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CandyShop.Models;
+using CandyShop.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Language;
+
+namespace CandyShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ShoppingCart _shoppingCart;
+        public OrderController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
