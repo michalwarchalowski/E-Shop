19dc422ba6e73d00526a493a762beaf310e20384
Michał Warchałowski <michalwarchalowski@gmail.com>
2020-05-30 08:33:09 +0200

    Creating Order and Order Detail Model

---
+++ CandyShop/CandyShop/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShop.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Address { get; set; }
+        public int City { get; set; }
+        public int State { get; set; }
+        public int ZipCode { get; set; }
+        public int PhoneNumber { get; set; }
+
+        public List<OrderDetail> OrderDetails { get; set; }
+
+        public decimal OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+    }
+}
